395fd38d7064bb3c5e80192305ba05451d607901
shreyas <sh117sh1178@gmail.com>
2025-09-09 14:12:35 +0530

    Minor Changes

--- 5_nQueens.cpp
+++ 5_nQueens.cpp
@@ -1,75 +1,55 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-void queen(vector<vector<bool>> &board, int row);
-bool isSafe(vector<vector<bool>> &board, int row, int col);
-void display(vector<vector<bool>> &board);
-
-void queen(vector<vector<bool>> &board, int row)
+void print(int board[], int n)
 {
-    if (row == board.size())
+    for (int i = 0; i < n; i++)
     {
-        display(board);
-        cout << "----------------------------------------\n";
-        return;
-    }
-    for (int col = 0; col < board.size(); col++)
-    {
-        if (isSafe(board, row, col))
+        for (int j = 0; j < n; j++)
         {
-            board[row][col] = true;
-            queen(board, row + 1);
-            board[row][col] = false;
+            if (board[i] == j)
+                cout << " Q ";
+            else
+                cout << " - ";
         }
+        cout << "\n";
     }
+    cout << "-----------------------------\n";
 }
 
-bool isSafe(vector<vector<bool>> &board, int row, int col)
+bool issafe(int board[], int row, int col)
 {
     for (int i = 0; i < row; i++)
     {
-        if (board[i][col])
-            return false;
-    }
-    
-    int maxLeft = min(row, col);
-    for (int i = 1; i <= maxLeft; i++)
-    {
-        if (board[row - i][col - i])
-            return false;
-    }
-
-    int maxRight = min(row, (int)board.size() - col - 1);
-    for (int i = 1; i <= maxRight; i++)
-    {
-        if (board[row - i][col + i])
+        if (board[i] == col || abs(board[i] - col) == abs(i - row))
             return false;
     }
     return true;
 }
 
-void display(vector<vector<bool>> &board)
+void nqueen(int board[], int row, int n)
 {
-    for (auto row : board)
+    if (row == n)
     {
-        for (auto element : row)
+        print(board, n);
+        return;
+    }
+    for (int col = 0; col < n; col++)
+    {
+        if (issafe(board, row, col))
         {
-            element ? cout << "Q " : cout << "- ";
+            board[row] = col;
+            nqueen(board, row + 1, n);
         }
-        cout << "\n";
     }
 }
 
 int main()
 {
     int n;
-    cout << "Enter the Board Size (NxN) : ";
+    cout << "Enter the number of queens : ";
     cin >> n;
-
-    vector<vector<bool>> board(n, vector<bool>(n, false));
-    queen(board, 0);
-    
+    int board[20];
+    nqueen(board, 0, n);
     return 0;
 }
